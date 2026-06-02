3a635cb89c8c74a64a24c92f07cb77aba8ccd779
Shoumyo <shoumyosrijito@gmail.com>
2026-06-02 07:21:41 +0600

    dynamic menu button toggle and menu toggle

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu } from 'lucide-react';
+import { SquareX } from 'lucide-react';
 
 const navigationData = [
   {
@@ -30,19 +32,36 @@ const navigationData = [
 ];
 
 const NavBar = () => {
+
+    const[open,setOpen]=useState(false);
+    const links=navigationData.map(route=><Link route={route}></Link>);
+
     return (
-        <nav>
+        <nav className='flex justify-between mx-10'>
             {/* <ul className='flex'>
                 <li className='mr-10'><a href=''>Home</a></li>
                 <li className='mr-10'><a href=''>About</a></li>
                 <li className='mr-10'><a href=''>Blog</a></li>
             </ul> */}
+            <span className='flex'onClick={()=>setOpen(!open)}>
+                <span className='md:hidden'>
+                    {open?<SquareX></SquareX>:<Menu></Menu>};
+                </span>
 
-            <ul className='flex'>
+                <ul className='md:hidden'>
+                    {
+                        links
+                    }
+                </ul>
+                
+                <h3 className='ml-4'>My navbar</h3>
+            </span>
+            <ul className='md:flex hidden'>
                 {
-                    navigationData.map(route=><Link route={route}></Link>)
+                 links
                 }
             </ul>
+            <button className='btn'>Sign In</button>
         </nav>
     );
 };
